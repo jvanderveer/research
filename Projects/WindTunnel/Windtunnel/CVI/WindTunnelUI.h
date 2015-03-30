8b716d3b389197ccdec2102441e6f65c12ebb190
Joseph VanderVeer <joseph.vanderveer@gmail.com>
2015-03-30 10:07:12 -0400

    Added the wind tunnel project files

---
+++ Projects/WindTunnel/Windtunnel/CVI/WindTunnelUI.h
@@ -0,0 +1,53 @@
+/**************************************************************************/
+/* LabWindows/CVI User Interface Resource (UIR) Include File              */
+/* Copyright (c) National Instruments 2015. All Rights Reserved.          */
+/*                                                                        */
+/* WARNING: Do not add to, delete from, or otherwise modify the contents  */
+/*          of this include file.                                         */
+/**************************************************************************/
+
+#include <userint.h>
+
+#ifdef __cplusplus
+    extern "C" {
+#endif
+
+     /* Panels and Controls: */
+
+#define  PANEL                            1
+#define  PANEL_uiAmbient                  2       /* control type: scale, callback function: (none) */
+#define  PANEL_uiHeater1                  3       /* control type: scale, callback function: (none) */
+#define  PANEL_uiJetSpeedIndicator        4       /* control type: scale, callback function: (none) */
+#define  PANEL_uiSpeedIndicator           5       /* control type: scale, callback function: (none) */
+#define  PANEL_uiHeater2                  6       /* control type: scale, callback function: (none) */
+#define  PANEL_uiHeater3                  7       /* control type: scale, callback function: (none) */
+#define  PANEL_TAB                        8       /* control type: tab, callback function: (none) */
+#define  PANEL_uiMotionIndicator          9       /* control type: LED, callback function: (none) */
+#define  PANEL_uiFlowIndicator            10      /* control type: LED, callback function: (none) */
+#define  PANEL_uiHeatIndicator            11      /* control type: LED, callback function: (none) */
+#define  PANEL_uiJetIndicator             12      /* control type: LED, callback function: (none) */
+#define  PANEL_uiHeatLockoutIndicato      13      /* control type: LED, callback function: (none) */
+#define  PANEL_STRIPCHART                 14      /* control type: strip, callback function: (none) */
+#define  PANEL_STRIPCHART_2               15      /* control type: strip, callback function: (none) */
+
+     /* tab page panel controls */
+#define  TABPANEL_uiSetNoFlow             2       /* control type: command, callback function: (none) */
+#define  TABPANEL_uiBarometric            3       /* control type: numeric, callback function: (none) */
+
+
+     /* Control Arrays: */
+
+          /* (no control arrays in the resource file) */
+
+
+     /* Menu Bars, Menus, and Menu Items: */
+
+          /* (no menu bars in the resource file) */
+
+
+     /* (no callbacks specified in the resource file) */ 
+
+
+#ifdef __cplusplus
+    }
+#endif
